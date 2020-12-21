ced33107fca4463030dc0685a3f4835a835beb66
Renat <suleymanov_renat@mail.ru>
2020-12-21 18:11:26 +0300

    Второй вариант

--- src/js/game.js
+++ src/js/game.js
@@ -14,3 +14,22 @@ export function readGameSaving() {
   
 export function writeGameSaving() {
   };
+
+  // Раскоментируйте этот код - он не будет работать. Почему?
+  // export default Game;
+  // export {GameSavingData, readGameSaving, writeGameSaving}
+
+  // class Game {
+  //   static start() {
+  //      console.log('game started');
+  //    }
+  //  };
+   
+  // class GameSavingData {
+  //  }
+   
+  // function readGameSaving() {
+  //  };
+   
+  // function writeGameSaving() {
+  //  };
